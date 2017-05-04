37c03bf7e37188d41c24aa80ddf102332a63e05a
Jaim Zuber <jaim@sharpfivesoftware.com>
2017-05-04 16:33:06 -0500

    WIP add delegate to retrieve basal settings

--- LoopKit/UI/BasalRatesTableViewController.swift
+++ LoopKit/UI/BasalRatesTableViewController.swift
@@ -7,10 +7,18 @@
 //
 
 import Foundation
+//import RileyLinkKit
+
+public protocol BasalRatesTableViewControllerDelegate {
+    func basalRatesUpdateRequested(by basalRatesViewController: BasalRatesTableViewController)
+}
 
 public class BasalRatesTableViewController : SingleValueScheduleTableViewController {
 
-    let ButtonReuseIdentifier = "BasalRabtesButtonReuseIdentifier"
+    let ButtonReuseIdentifier = "BasalRatesButtonReuseIdentifier"
+    public var basalRateDelegate: BasalRatesTableViewControllerDelegate?
+//    public var device: RileyLinkDevice?
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -34,9 +42,34 @@ public class BasalRatesTableViewController : SingleValueScheduleTableViewControl
     
     public override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == self.tableView(tableView, numberOfRowsInSection: indexPath.section) - 1 {
-            //
+//            updateBasalRatesFromDevice()
+            
+            basalRateDelegate?.basalRatesUpdateRequested(by: self)
         } else {
             super.tableView(tableView, didSelectRowAt: indexPath)
         }
+    }
+    
+    func updateBasalRatesFromDevice() {
+//        device?.ops?.getBasalSettings({ (basalScheduleResponse) in
+//            
+//            switch basalScheduleResponse {
+//                
+//            case .success(let basalReponseArray):
+//                
+//                // Convert to displayable type
+//                let displayableTypeArray = basalReponseArray.map{ return RepeatingScheduleValue<Double>(startTime: TimeInterval(minutes: Double($0.minutes)), value: $0.rate) }
+//                
+//                self.scheduleItems = displayableTypeArray
+//                
+//                DispatchQueue.main.async {
+//                    self.tableView.reloadData()
+//                }
+//            case .failure(let error):
+//                NSLog("Error updating Basal Settings: \(String(describing: error))")
+//                break
+//            
+//            }
+//        })
     }
 }
