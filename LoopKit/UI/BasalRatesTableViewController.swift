7635a587aacdf64a1da89587bd62ebe5958f3cf9
Jaim Zuber <jaim@sharpfivesoftware.com>
2017-05-05 14:18:27 -0500

    Fixup - remove RileyLink dependencies

--- LoopKit/UI/BasalRatesTableViewController.swift
+++ LoopKit/UI/BasalRatesTableViewController.swift
@@ -17,12 +17,10 @@ public class BasalRatesTableViewController : SingleValueScheduleTableViewControl
 
     let ButtonReuseIdentifier = "BasalRatesButtonReuseIdentifier"
     public var basalRateDelegate: BasalRatesTableViewControllerDelegate?
-//    public var device: RileyLinkDevice?
     
     public override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
     
     public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let superRows = super.tableView(tableView, numberOfRowsInSection: section)
@@ -30,10 +28,9 @@ public class BasalRatesTableViewController : SingleValueScheduleTableViewControl
     }
     
     public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         if indexPath.row == self.tableView(tableView, numberOfRowsInSection: indexPath.section) - 1 {
             let buttonCell = UITableViewCell(style: .default, reuseIdentifier: ButtonReuseIdentifier)
-            buttonCell.textLabel?.text = "Button - not really"
+            buttonCell.textLabel?.text = "Update Basal Rates"
             return buttonCell
         } else {
             return super.tableView(tableView, cellForRowAt: indexPath)
@@ -42,34 +39,10 @@ public class BasalRatesTableViewController : SingleValueScheduleTableViewControl
     
     public override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == self.tableView(tableView, numberOfRowsInSection: indexPath.section) - 1 {
-//            updateBasalRatesFromDevice()
             
             basalRateDelegate?.basalRatesUpdateRequested(by: self)
         } else {
             super.tableView(tableView, didSelectRowAt: indexPath)
         }
-    }
-    
-    func updateBasalRatesFromDevice() {
-//        device?.ops?.getBasalSettings({ (basalScheduleResponse) in
-//            
-//            switch basalScheduleResponse {
-//                
-//            case .success(let basalReponseArray):
-//                
-//                // Convert to displayable type
-//                let displayableTypeArray = basalReponseArray.map{ return RepeatingScheduleValue<Double>(startTime: TimeInterval(minutes: Double($0.minutes)), value: $0.rate) }
-//                
-//                self.scheduleItems = displayableTypeArray
-//                
-//                DispatchQueue.main.async {
-//                    self.tableView.reloadData()
-//                }
-//            case .failure(let error):
-//                NSLog("Error updating Basal Settings: \(String(describing: error))")
-//                break
-//            
-//            }
-//        })
     }
 }
